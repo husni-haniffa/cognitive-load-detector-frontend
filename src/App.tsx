fa441dab5d5ab3996495050cb9a3d46cb9502ec6
Husni <husniwfayo@gmail.com>
2025-08-21 02:26:24 +0530

    initial phase of ui implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Badge } from "./components/ui/badge"
+import CircularProgress from "./components/ui/circular-progress"
+import { Label } from "./components/ui/label"
+import MetricCard from "./components/ui/mertric-card"
+import MetricsTable from "./components/ui/metric-table"
+import { useGetCognitiveStateHistoryQuery, useGetLatestCognitiveStateQuery } from "./features/cognitive-state-API"
+import Navbar from "./Navbar"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const { data: latest, isLoading: isLatestLoading, isError: isLatestError } =
+    useGetLatestCognitiveStateQuery()
+  const { data: history, isLoading: isHistoryLoading, isError: isHistoryError } =
+    useGetCognitiveStateHistoryQuery()
+
+  if (isLatestLoading || isHistoryLoading) return <p>Loading</p>
+  if (isLatestError || isHistoryError) return <p>error</p>
+   
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar/>
+      <div className="p-24">
+
+        <section className="border-b-2 border-black pb-2">
+          <header className="flex justify-between items-start">
+            <div>
+              <Label className="text-xl">Current Cognitive State</Label>
+              <Label>Latest agregrated metrics score</Label>
+            </div>
+            <Label>Last Updated 4.35 PM</Label>
+          </header>
+        </section>
+
+        <section className="mt-12 grid grid-cols-2">
+          <div className="bg-red-300">
+            <CircularProgress state={latest.cognitive_state.label} score={latest.cognitive_state.score}/>
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            <MetricCard title="Facial Cue Data" values={latest.facial_cue_data} />
+            <MetricCard title="Keystroke Data" values={latest.keystroke_data} />
+          </div>
+        </section>
+
+        <section className="mt-12">
+            <MetricsTable data={history}/>
+        </section>
+
+    
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      
+      
     </>
+    
   )
 }
 
---
+++ src/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Brain, Play, Square } from "lucide-react"
+import { Label } from "./components/ui/label"
+import { Button } from "./components/ui/button"
+
+
+const Navbar = () => {
+  return (
+    <nav className="shadow-2xl p-4">
+        <div className="flex items-center justify-between">
+            <div>
+                <header className="flex gap-3">
+                    <Brain size={65} className="text-purple-400"/>
+                    <Label className="text-4xl font-bold">Cognitive Load Detection</Label>   
+                </header>
+                <div>
+                    <span className="mx-20">Real Time Monitoring Dashboard</span>
+                </div>
+            </div>   
+            <div className="flex gap-3">
+                <Button className="bg-green-600 hover:bg-green-500 font-bold" size="lg">
+                    <span><Play strokeWidth={4}/></span>Start
+                </Button>
+                <Button className="bg-red-600 hover:bg-red-500 font-bold" size="lg">
+                    <span><Square strokeWidth={4}/></span>Stop
+                </Button>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
